6a13bd02e88ad562f1f6946075901000b906408b
herrybiscuit <herrybiscuit@gmail.com>
2014-04-24 02:24:14 -0400

    Added F2 key hide/show
    when f2 is pressed the cursor is now hidden/shown

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -1,1 +1,9 @@
 include("shared.lua")
+
+--Show/hide mouse cursor when f2 is pressed
+local function KeyPress()
+    gui.EnableScreenClicker(input.IsKeyDown(KEY_F2))
+end
+
+--Add hooks
+hook.Add("Think", "Clients_Key_F2" ,KeyPress)
